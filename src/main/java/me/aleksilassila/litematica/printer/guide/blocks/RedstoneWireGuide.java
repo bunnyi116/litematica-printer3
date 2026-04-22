d98e52ec070efbc59a8311ba21007a612d250792
bunnyi116 <1160706050@qq.com>
2026-04-23 01:01:07 +0800

    修复红石线自动调整

--- src/main/java/me/aleksilassila/litematica/printer/guide/blocks/RedstoneWireGuide.java
+++ src/main/java/me/aleksilassila/litematica/printer/guide/blocks/RedstoneWireGuide.java
@@ -5,6 +5,7 @@ import me.aleksilassila.litematica.printer.guide.Guide;
 import me.aleksilassila.litematica.printer.printer.SchematicBlockContext;
 import me.aleksilassila.litematica.printer.printer.action.Action;
 import me.aleksilassila.litematica.printer.printer.action.ClickAction;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.RedstoneSide;
 import org.jetbrains.annotations.Nullable;
@@ -13,41 +14,37 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * 红石线交互指南。
- * 注册到：RedStoneWireBlock.class
- *
- * <p>WRONG_STATE：十字形红石线可点击变成点状。
+ * 红石线
  */
 public class RedstoneWireGuide extends Guide {
-
-    /** 红石线四方连接状态 */
-    private final @Nullable RedstoneSide northRedstone;
-    private final @Nullable RedstoneSide eastRedstone;
-    private final @Nullable RedstoneSide southRedstone;
-    private final @Nullable RedstoneSide westRedstone;
-
     public RedstoneWireGuide(SchematicBlockContext context) {
         super(context);
-        this.northRedstone = getProperty(requiredState, BlockStateProperties.NORTH_REDSTONE).orElse(null);
-        this.eastRedstone = getProperty(requiredState, BlockStateProperties.EAST_REDSTONE).orElse(null);
-        this.southRedstone = getProperty(requiredState, BlockStateProperties.SOUTH_REDSTONE).orElse(null);
-        this.westRedstone = getProperty(requiredState, BlockStateProperties.WEST_REDSTONE).orElse(null);
     }
 
     @Override
     protected Optional<Action> onBuildActionWrongState(BlockMatchResult state, AtomicReference<Boolean> skipOtherGuide) {
-        boolean allNoneRequired = northRedstone == RedstoneSide.NONE
-                && southRedstone == RedstoneSide.NONE
-                && eastRedstone == RedstoneSide.NONE
-                && westRedstone == RedstoneSide.NONE;
+        RedstoneSide rNorth = requiredState.getValue(BlockStateProperties.NORTH_REDSTONE);
+        RedstoneSide rEast = requiredState.getValue(BlockStateProperties.EAST_REDSTONE);
+        RedstoneSide rSouth = requiredState.getValue(BlockStateProperties.SOUTH_REDSTONE);
+        RedstoneSide rWest = requiredState.getValue(BlockStateProperties.WEST_REDSTONE);
 
-        boolean allSideCurrent = northRedstone == RedstoneSide.SIDE
-                && southRedstone == RedstoneSide.SIDE
-                && eastRedstone == RedstoneSide.SIDE
-                && westRedstone == RedstoneSide.SIDE;
+        RedstoneSide cNorth = currentState.getValue(BlockStateProperties.NORTH_REDSTONE);
+        RedstoneSide cEast = currentState.getValue(BlockStateProperties.EAST_REDSTONE);
+        RedstoneSide cSouth = currentState.getValue(BlockStateProperties.SOUTH_REDSTONE);
+        RedstoneSide cWest = currentState.getValue(BlockStateProperties.WEST_REDSTONE);
+
+        boolean allNoneRequired = rNorth == RedstoneSide.NONE
+                && rSouth == RedstoneSide.NONE
+                && rEast == RedstoneSide.NONE
+                && rWest == RedstoneSide.NONE;
+
+        boolean allSideCurrent = cNorth == RedstoneSide.SIDE
+                && cSouth == RedstoneSide.SIDE
+                && cEast == RedstoneSide.SIDE
+                && cWest == RedstoneSide.SIDE;
 
         if (allNoneRequired && allSideCurrent) {
-            return Optional.of(new ClickAction().setItem(net.minecraft.world.item.Items.AIR));
+            return Optional.of(new ClickAction().setItem(Items.AIR));
         }
         return Optional.empty();
     }
