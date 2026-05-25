b2e90f29104ff12150056708df406e3e71962785
bunnyi116 <1160706050@qq.com>
2026-05-25 15:58:31 +0800

    fix: 移除 MixinLocalPlayer.tick() 中 BlockPosCooldownUtils.tick() 重复调用

--- src/main/java/me/aleksilassila/litematica/printer/mixin/printer/mc/MixinLocalPlayer.java
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/printer/mc/MixinLocalPlayer.java
@@ -62,7 +62,6 @@ public class MixinLocalPlayer extends AbstractClientPlayer {
     @Inject(at = @At("HEAD"), method = "tick")
     public void tick(CallbackInfo ci) {
         BlockPosCooldownUtils.INSTANCE.tick();
-        BlockPosCooldownUtils.INSTANCE.tick();
         InventoryUtils.tick();
         ZxyUtils.tick();
         if (minecraft.gameMode instanceof MultiPlayerGameModeExtension extension) {
