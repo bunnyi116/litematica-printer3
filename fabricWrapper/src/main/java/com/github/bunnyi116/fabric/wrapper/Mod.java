4d0c036e6273ca76df02220bc9248f18d4d84467
bunnyi116 <1160706050@qq.com>
2026-07-19 15:34:55 +0800

    修改提示词

--- fabricWrapper/src/main/java/com/github/bunnyi116/fabric/wrapper/Mod.java
+++ fabricWrapper/src/main/java/com/github/bunnyi116/fabric/wrapper/Mod.java
@@ -7,7 +7,7 @@ public class Mod implements ModInitializer{
     @Override
     public void onInitialize() {
         if (!FabricLoader.getInstance().isModLoaded("litematica-printer")){
-            throw new RuntimeException("Wrapper failed to initialize core module. Please update litematica-printer and its dependencies.");
+            throw new RuntimeException("Wrapper failed to load core printer module. Please update litematica-printer, malilib and litematica to versions compatible with your current game version.");
         }
     }
 }
